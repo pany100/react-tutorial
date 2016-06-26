1a3337420141ddbd2a01b4767ad757e1df62235f
lpaniagua <lpaniagua@magnetico.com.ar>
2016-06-26 12:14:23 -0300

    change styles of home

--- app/components/Home.js
+++ app/components/Home.js
@@ -1,10 +1,18 @@
 var React = require('react');
+var transparentBg = require('../styles').transparentBg;
+var ReactRouter = require('react-router');
+var Link = ReactRouter.Link;
 
 var Home = React.createClass({
-    displayName: 'Home',
     render() {
         return (
-            <div>Hello from Home</div>
+          <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+            <h1>Github Battle</h1>
+            <p className="lead">Some fancy motto</p>
+            <Link to="/playerOne">
+              <button className="btn btn-lg btn-success" type="button">Get Started</button>
+            </Link>
+          </div>
         );
     }
 });
